c76f1cff959983a090722966c7ee77dda362dca3
Ajay Yadav <ajaywebmaster01@gmail.com>
2026-04-01 16:22:46 +0530

    fix: dynamic port for Render

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+const PORT = process.env.PORT || 5000;
 
 // Rate limiter for OTP
 const otpLimiter = rateLimit({
@@ -26,6 +27,6 @@ app.get('/',(req,res)=>{
 
 
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log("Server running on port 5000");
 });
